f1e9eff96ec6810ffad5711d9021b0818fa5864c
Dima <jkhdjfjkjshjb@gmail.com>
2022-02-05 20:45:40 +0300

    Add project files.

---
+++ CSharpLab/Money.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLab
+{
+    public delegate double MoneyConvertDelegate(double value);
+    internal class Money
+    {
+        private string CurrencyCode { get; set; }
+        private double Amount { get; set; }
+
+        public Money(double amount, string currencyCode)
+        {
+            Amount = amount;
+            CurrencyCode = currencyCode;
+        }
+        public override string ToString()
+        {
+            return $"У Вас {Amount} {CurrencyCode}";
+        }
+        public  void Convert (MoneyConvertDelegate del)
+        {
+            foreach (MoneyConvertDelegate value in del.GetInvocationList())
+            {
+
+                Console.WriteLine("Пересчитываю ...");
+                value(Amount);
+
+            }
+        }
+    }
+}
---
+++ CSharpLab/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Money curr = new Money(7567, "rub");
+            Money dol = new Money(2.57, "$");
+            MoneyConvertDelegate del = null;
+            del += delegate (double amount)
+            {
+                Console.WriteLine($"{amount} руб = {amount * 0.013} $");
+                return amount;
+            };
+            del += delegate (double amount)
+              {
+                  Console.WriteLine($"{amount} руб = {amount * 0.012} евро");
+                  return amount;
+              };
+            del += delegate (double amount)
+              {
+                  Console.WriteLine($"{amount} руб = {amount * 0.32} гондурасских лемпир");
+                  return amount;
+              }; 
+            del += (double amount) =>
+              {
+                  Console.WriteLine($"{amount} руб = {amount * 0.000007} унций золота");
+                  return amount;
+              };
+
+            curr.Convert(del);
+            
+
+        }
+    }
+}
